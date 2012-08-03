4ed7dc7d721502b020c0c0afbe71b5062a26f9dd
Arthur Andersen <leoc.git@gmail.com>
2012-08-03 17:09:16 +0200

    add brackets to mode line

--- org-pomodoro.el
+++ org-pomodoro.el
@@ -96,13 +96,16 @@ a pomodoro and to :longbreak or :break when starting a break.")
 (defun org-pomodoro-update-mode-line ()
   "Sets the modeline accordingly to the current state."
   (setq org-pomodoro-mode-line
-        (propertize (format (case org-pomodoro-state
-                              (:none "")
-                              (:pomodoro org-pomodoro-format)
-                              (:short-break org-pomodoro-short-break-format)
-                              (:long-break org-pomodoro-long-break-format))
-                            (org-pomodoro-minutes))
-                    'face 'org-pomodoro-mode-line))
+        (list
+         "["
+         (propertize (format (case org-pomodoro-state
+                               (:none "")
+                               (:pomodoro org-pomodoro-format)
+                               (:short-break org-pomodoro-short-break-format)
+                               (:long-break org-pomodoro-long-break-format))
+                             (org-pomodoro-minutes))
+                     'face 'org-pomodoro-mode-line)
+         "] "))
   (force-mode-line-update))
 
 
