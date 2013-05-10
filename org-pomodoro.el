bdaf5c9c56a848eabe35915f0e7fbf72be138422
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-05-10 20:36:59 +0200

    Correct commentary

--- org-pomodoro.el
+++ org-pomodoro.el
@@ -24,8 +24,8 @@
 ;; Org-pomodoro introduces an easy way to clock time in org-mode with
 ;; the pomodoro technique.  You can clock into tasks with starting a
 ;; pomodoro time automatically.  Each finished pomodoro is followed by
-;; a pause timer.  If you completed 4 pomodoros in a row the pause is
-;; longer that the shorter pauses between each pomodoro.
+;; a break timer.  If you completed 4 pomodoros in a row the break is
+;; longer that the shorter break between each pomodoro.
 ;;
 ;; For a full explanation of the pomodoro technique, have a look at:
 ;;   http://www.pomodorotechnique.com
