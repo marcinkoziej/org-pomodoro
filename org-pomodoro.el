24422c5b1010a4cc565c75d0203bdd019b871a22
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-11-16 16:00:16 +0100

    Use cl-case instead of pcase

--- org-pomodoro.el
+++ org-pomodoro.el
@@ -188,11 +188,11 @@ or :break when starting a break.")
 
 (defun org-pomodoro-play-sound (type)
   "Play an audio file specified by TYPE (:pomodoro, :short-break, :long-break)."
-  (let ((sound (pcase type
+  (let ((sound (cl-case type
                  (:pomodoro org-pomodoro-sound)
                  (:short-break org-pomodoro-short-break-sound)
                  (:long-break org-pomodoro-long-break-sound)
-                 (_ (error "Unknown org-pomodoro sound: %S" type)))))
+                 (t (error "Unknown org-pomodoro sound: %S" type)))))
     (when (and org-pomodoro-play-sounds sound org-pomodoro-audio-player)
       (call-process org-pomodoro-audio-player nil 0 nil (expand-file-name sound)))))
 
