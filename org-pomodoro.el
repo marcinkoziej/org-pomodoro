9b240e9a229a960065c74eac101f552e0cec9f62
Arthur Andersen <leoc.git@gmail.com>
2015-05-30 16:17:45 +0200

    Bump version number to 2.0.0

--- org-pomodoro.el
+++ org-pomodoro.el
@@ -2,7 +2,7 @@
 
 ;; Author: Arthur Leonard Andersen <leoc.git@gmail.com>, Marcin Koziej <marcin at lolownia dot org>
 ;; Created: May 10, 2013
-;; Version: 1.7.1
+;; Version: 2.0.0
 ;; Package-Requires: ((alert "0.5.10") (cl-lib "0.5"))
 
 ;; This file is free software; you can redistribute it and/or modify
